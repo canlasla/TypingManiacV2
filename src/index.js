306d21dfe4f798d1c8a37dd3023e53544907d288
canlasla <lancecanlas25@gmail.com>
2021-02-07 23:15:37 -0500

    Read words from array and can display them to canvas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import Game from '/src/game';
-
+import { wordArray } from '/src/wordsarray.js';
+// var test = array;
 var canvas = document.getElementById('gameScreen');
 var ctx = canvas.getContext('2d');
 canvas.width = canvas.getBoundingClientRect().width;
@@ -9,9 +10,12 @@ const GAME_WIDTH = canvas.getBoundingClientRect().width;
 const GAME_HEIGHT = canvas.getBoundingClientRect().height;
 
 // let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+console.log(wordArray);
 
-var x = 50;
-var y = 50;
+// var x = Math.floor(Math.random() * GAME_WIDTH - 50);
+var word = wordArray[Math.floor(Math.random() * wordArray.length)];
+var x = Math.floor(Math.random() * (GAME_WIDTH - ctx.measureText(word).width));
+var y = 0;
 let lastTime = 0;
 function gameLoop(timestamp) {
 	let deltaTime = timestamp - lastTime;
@@ -24,10 +28,15 @@ function gameLoop(timestamp) {
 
 	ctx.font = '20px Consolas';
 	ctx.fillStyle = 'white';
-	ctx.fillText('Hello World', x, y);
-	x++;
-	y++;
+	ctx.fillText(word, x, y);
+	y += 10;
 
+	if (y > GAME_HEIGHT) {
+		x = Math.floor(Math.random() * (GAME_WIDTH - ctx.measureText(word).width));
+		y = 0;
+
+		word = wordArray[Math.floor(Math.random() * wordArray.length)];
+	}
 	requestAnimationFrame(gameLoop);
 }
 
